9cfb82a05b9c92dc8e55d2a6568f8f35f22fbaf5
Christopher Alphonse <christopheralphonse96@gmail.com>
2022-05-15 10:22:36 -0400

    c

--- client/src/components/MakeProducts.jsx
+++ client/src/components/MakeProducts.jsx
@@ -1,9 +1,5 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-
-
-
-
 
 const MakeProducts = (props) => {
   const [title, setTitle] = useState("");
@@ -14,17 +10,42 @@ const MakeProducts = (props) => {
     <main>
       <header>Product Manager</header>
       <form>
-<section className="form-fields">
+        <section>
+          <label htmlFor="title"> Title </label>
+          <input
+            type="text"
+            value={title}
+            name="title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </section>
 
+        <section>
+          <label htmlFor="price"> Price </label>
+          <input
+            type="number"
+            value={price}
+            name="price"
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+        </section>
 
-
-
-
-</section>
-
-
-
-
+        <section>
+          <label htmlFor="description">Description </label>
+          <input
+            type="text"
+            value={description}
+            name="description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </section>
+        <button type="submit"> Update </button>
       </form>
     </main>
   );
